7b6a52661d7a359f8c5536aa90d346e2b4ced4be
shalomTalker <shalom604@gmail.com>
2019-05-02 11:30:24 +0300

    prod

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            fib calculator
           </a>
         </header>
           <div>
